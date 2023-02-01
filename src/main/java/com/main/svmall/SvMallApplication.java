2e50b3addace7b841d0320bc8269bcf642cbf43f
“sv-zlf” <1375451299@qq.com>
2023-02-01 15:56:18 +0800

    Initial commit

---
+++ src/main/java/com/main/svmall/SvMallApplication.java
@@ -0,0 +1,13 @@
+package com.main.svmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/main/svmall/SvMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.svmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
