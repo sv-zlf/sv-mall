34ef5bdae1104d4313b480d3a37f97f54892e559
“sv-zlf” <1375451299@qq.com>
2023-02-03 16:42:37 +0800

    项目框架搭建

---
+++ svmall-gateway-user/src/test/java/com/svmall/gatewayuser/SvmallGatewayUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svmall.gatewayuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvmallGatewayUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
