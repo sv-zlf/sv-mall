584d7a06da2f7c92335a809eea6e31dc4b26f587
“sv-zlf” <1375451299@qq.com>
2023-05-25 20:55:55 +0800

    👊nacos动态路由

--- svmall-gateway-admin/src/main/java/com/svmall/gatewayadmin/config/SwaggerResourceConfig.java
+++ svmall-gateway-admin/src/main/java/com/svmall/gatewayadmin/config/SwaggerResourceConfig.java
@@ -1,51 +1,63 @@
 package com.svmall.gatewayadmin.config;
 
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.gateway.config.GatewayProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.support.NameUtils;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import springfox.documentation.swagger.web.SwaggerResource;
 import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-@Slf4j
 @Component
-@Primary
-@AllArgsConstructor
 public class SwaggerResourceConfig implements SwaggerResourcesProvider {
 
-    private final RouteLocator routeLocator;
-    private final GatewayProperties gatewayProperties;
+    /**
+     * 接口地址
+     */
+    public static final String API_URI = "/v2/api-docs";
 
+    /**
+     * 路由加载器
+     */
+    @Autowired
+    private RouteLocator routeLocator;
+
+    /**
+     * 网关应用名称
+     */
+    @Value("${spring.application.name}")
+    private String applicationName;
 
     @Override
     public List<SwaggerResource> get() {
+        //接口资源列表
         List<SwaggerResource> resources = new ArrayList<>();
-        List<String> routes = new ArrayList<>();
-        routeLocator.getRoutes().subscribe(route -> routes.add(route.getId()));
-        gatewayProperties.getRoutes().stream().filter(routeDefinition -> routes.contains(routeDefinition.getId())).forEach(route -> {
-            route.getPredicates().stream()
-                    .filter(predicateDefinition -> ("Path").equalsIgnoreCase(predicateDefinition.getName()))
-                    .forEach(predicateDefinition -> resources.add(swaggerResource(route.getId(),
-                            predicateDefinition.getArgs().get(NameUtils.GENERATED_NAME_PREFIX + "0")
-                                    .replace("**", "v2/api-docs"))));
+        //服务名称列表
+        List<String> routeHosts = new ArrayList<>();
+        // 获取所有可用的应用名称
+        routeLocator.getRoutes().filter(route -> route.getUri().getHost() != null)
+                .filter(route -> !applicationName.equals(route.getUri().getHost()))
+                .subscribe(route -> routeHosts.add(route.getUri().getHost()));
+        // 去重，多负载服务只添加一次
+        Set<String> existsServer = new HashSet<>();
+        routeHosts.forEach(host -> {
+            // 拼接url
+            String url = "/" + host + API_URI;
+            //不存在则添加
+            if (!existsServer.contains(url)) {
+                existsServer.add(url);
+                SwaggerResource swaggerResource = new SwaggerResource();
+                swaggerResource.setUrl(url);
+                swaggerResource.setName(host);
+                resources.add(swaggerResource);
+            }
         });
-
         return resources;
     }
 
-    private SwaggerResource swaggerResource(String name, String location) {
-        log.info("name:{},location:{}",name,location);
-        SwaggerResource swaggerResource = new SwaggerResource();
-        swaggerResource.setName(name);
-        swaggerResource.setLocation(location);
-        swaggerResource.setSwaggerVersion("2.0");
-        return swaggerResource;
-    }
 }
 
